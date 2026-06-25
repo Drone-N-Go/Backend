daff5c14837694db37eba3ee561b9ed89c16406d
jamesmcdougall <james.mcdougall@saltandlighttechnologies.com>
2026-06-25 17:22:33 -0500

    made revision fixes

--- API/app/db/startup.py
+++ API/app/db/startup.py
@@ -12,7 +12,7 @@ from sqlalchemy.ext.asyncio import create_async_engine
 
 logger = logging.getLogger(__name__)
 
-EXPECTED_ADMIN_REVISION = "20260617_0007"
+EXPECTED_ADMIN_REVISION = "20260625_0008"
 EXPECTED_ADMIN_ROLE_CONSTRAINT_VALUES = {
     "owner",
     "master_developer",
